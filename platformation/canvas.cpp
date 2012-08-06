eb27af1a73012c8b7d0a8cb4eccf188ef1fa8b6b
Luke Benstead <kazade@gmail.com>
2012-08-06 21:52:05 +0100

    Update to use newest KGLT. Crashes mysteriously on startup

--- platformation/canvas.cpp
+++ platformation/canvas.cpp
@@ -5,11 +5,11 @@ namespace pn {
 Canvas::Canvas(BaseObjectType *cobject, const Glib::RefPtr<Gtk::Builder>& builder):
     GtkGLWidget(cobject) {
 
-    selection_.reset(new kglt::SelectionRenderer(scene()));
+    selection_.reset(new kglt::SelectionRenderer());
 
     scene().remove_all_passes();
     scene().add_pass(selection_);
-    scene().add_pass(kglt::Renderer::ptr(new kglt::GenericRenderer(scene())));
+    scene().add_pass(kglt::Renderer::ptr(new kglt::GenericRenderer()));
 
     signal_button_press_event().connect(
         sigc::mem_fun(this, &Canvas::mouse_button_pressed_cb)
@@ -33,9 +33,9 @@ void Canvas::do_resize(int width, int height) {
     set_height(get_allocation().get_height());
 
     scene().pass(0).viewport().set_size(width, height);
-    ortho_width_ = scene().pass(0).renderer().set_orthographic_projection_from_height(15.0, double(width) / double(height));
+    ortho_width_ = scene().active_camera().set_orthographic_projection_from_height(15.0, double(width) / double(height));
     scene().pass(1).viewport().set_size(width, height);
-    scene().pass(1).renderer().set_orthographic_projection_from_height(15.0, double(width) / double(height));
+    //scene().pass(1).renderer().set_orthographic_projection_from_height(15.0, double(width) / double(height));
 }
 
 bool Canvas::mouse_button_pressed_cb(GdkEventButton* event) {
